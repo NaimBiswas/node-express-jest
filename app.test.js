277d4ef67c5609e6d7a718e3ebd45caf886fa1a7
NaimBiswas <nayeembiswas2@gmail.com>
2022-07-03 11:42:14 +0600

    Learn jest for post api

--- app.test.js
+++ app.test.js
@@ -1,14 +1,28 @@
-const request = require('supertest')
-const app = require('./app')
+const request = require("supertest");
+const app = require("./app");
 
-describe('Root Api', () => {
-    it("GET / --> Root API", () => {
-        return request(app).get('/').expect('Content-Type', /json/).expect(200).then(res =>{
-            expect(res.body).toEqual(expect.objectContaining({
-                message: expect.any(String),
-                status:expect.any(String)
-            }))
-        })
-    })
-
+describe("Root Api", () => {
+  it("GET / --> Root API", () => {
+    return request(app)
+      .get("/")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((res) => {
+        expect(res.body).toEqual(
+          expect.objectContaining({
+            message: expect.any(String),
+            status: expect.any(String),
+          })
+        );
+      });
+  });
+  it("should create a new post", async () => {
+    const res = await request(app)
+      .post("/")
+      .send({
+        name: expect.any(String),
+        email: expect.any(String),
+      });
+    expect(res.statusCode).toBe(201);
+  });
 });
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get("/", function (req, res, next) {
   res.status(200).json({
-    message:"Welcome to the express js server",
-    status:"success"
+    message: "Welcome to the express js server",
+    status: "success",
+  });
+});
+router.post("/", (req, res) => {
+  res.status(201).json({
+    name: "Naim Biswas",
+    email: "nayeembiswas30@gmail.com",
   });
 });
 
